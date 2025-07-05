15c64878bce72b4fc01218ed5b476e78e0872e57
Hector Soto <hector_17rock@hotmail.com>
2025-07-05 01:16:58 -0400

    TASK05 Part 1 to 5

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -14,6 +15,9 @@ bcrypt = Bcrypt()
 
 # Initialize JWT instance
 jwt = JWTManager()
+
+# Initialize SQLAlchemy instance
+db = SQLAlchemy()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -36,6 +40,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     # Initialize Flask extensions
     bcrypt.init_app(app)
     jwt.init_app(app)
+    db.init_app(app)
 
     # Initialize Flask-RESTX API
     api = Api(app, version='1.0', title='HBnB API',
