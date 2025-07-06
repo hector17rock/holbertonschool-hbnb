dfe12e502d0acbfbb342b05529f6c2ef07e8e207
Hector Soto <hector_17rock@hotmail.com>
2025-07-06 13:42:30 -0400

    CORRECTIOS

---
+++ part3/tests/__init__.py
@@ -0,0 +1,10 @@
+"""
+Test package for HBnB application.
+
+This package contains all test modules for the HBnB application including:
+- Unit tests for models
+- Integration tests for API endpoints
+- CRUD operation tests
+- Business logic validation tests
+- SQLAlchemy model tests
+"""
