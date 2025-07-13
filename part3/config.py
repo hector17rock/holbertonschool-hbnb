9f44d43ee0b9cdc47657763fc7168453e1d49ac4
Hector Soto <hector_17rock@hotmail.com>
2025-07-13 19:05:34 -0400

    TASK0

--- part3/config.py
+++ part3/config.py
@@ -1,13 +1,29 @@
 import os
 
 class Config:
+    """Base configuration class"""
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
+    TESTING = False
 
 class DevelopmentConfig(Config):
+    """Development configuration"""
     DEBUG = True
+    DEVELOPMENT = True
+
+class ProductionConfig(Config):
+    """Production configuration"""
+    DEBUG = False
+    TESTING = False
+
+class TestingConfig(Config):
+    """Testing configuration"""
+    DEBUG = True
+    TESTING = True
 
 config = {
     'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig,
     'default': DevelopmentConfig
 }
